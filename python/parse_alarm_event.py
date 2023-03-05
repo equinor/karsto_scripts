4761d58f94d56888089bf2b6ac842d5b28eef112
Roy Vegard Ovesen <roy.vegard.ovesen@gmail.com>
2023-03-05 19:26:53 +0100

    Use curl to bulk index the data

--- python/parse_alarm_event.py
+++ python/parse_alarm_event.py
@@ -1,15 +1,23 @@
 import json
 import os
+import subprocess
 import zipfile
 
 events = []
 event_count = 0
 
-path = "C:/temp/alarm-manager/archive"
+path = "archive"
 files = os.listdir(path)
+number_of_files = len(files)
+current_file_number = 1
+command=["curl", "-H", r'Content-Type: application/x-ndjson', "-XPOST", r'https://localhost:9200/_bulk?pretty', "--cacert", r"/mnt/ub/home/royvegard/elastic_stack/elasticsearch-8.6.2/config/certs/http_ca.crt", "-u", r"elastic:qVcMGZ7Kw9Qsic=ykvXg", "--data-binary", r"@out.json"]
+index = { "create" : { "_index" : "sas-event-01" } }
+
+
 for f in files:
     full_path = os.path.join(path, f)
-    print(full_path)
+    print("file %d of %d: %s " % (current_file_number, number_of_files, full_path))
+
     with zipfile.ZipFile(full_path) as myzip:
         for file in myzip.namelist():
             with myzip.open(file) as datafile:
@@ -30,14 +38,35 @@ for f in files:
                     event['ActiveTime'] = event['ActiveTime'].replace(
                         " ", "T", 1)
 
-                    print(json.dumps(event, indent=4))
                     events.append(event)
+                    event_count = event_count + 1
 
-                    if len(events) > 100000:
-                        event_count = event_count + len(events)
-                        print(event_count)
-                        # TODO Output the collected events
+                    if len(events) > 90000:
+                        print("events: ", event_count)
+                        with open("out.json", "w", encoding="utf-8") as out_file:
+                            for ev in events:
+                                out_file.write(json.dumps(index, ensure_ascii=False) + "\n")
+                                out_file.write(json.dumps(ev, ensure_ascii=False) + "\n")
+
                         events.clear()
-                    # print('#')
+                        process = subprocess.run(args=command, text=True, capture_output=True)
 
-    print(len(events))
+                        error = process.stdout.find('"errors" : true')
+                        if error > -1:
+                            print("Errors! Sequence:", j['Sequence'])
+                            #print(process.stdout)
+
+    if len(events) > 0:
+        print("remaining events: ", len(events))
+        with open("out.json", "w", encoding="utf-8") as out_file:
+            for ev in events:
+                out_file.write(json.dumps(index, ensure_ascii=False) + "\n")
+                out_file.write(json.dumps(ev, ensure_ascii=False) + "\n")
+
+        events.clear()
+        process = subprocess.run(args=command, text=True, capture_output=True)
+        error = process.stdout.find('"errors" : true')
+        if error > -1:
+            print("Errors! Sequence:", j['Sequence'])
+
+print("Done! Total events: ", event_count)
