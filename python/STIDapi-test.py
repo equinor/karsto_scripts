e3898312d2c7ea4894cb1c19c251ff6083d58c28
Roy Vegard Ovesen <rovo@equinor.com>
2023-11-21 08:57:05 +0100

    Token as agument

--- python/STIDapi-test.py
+++ python/STIDapi-test.py
@@ -1,3 +1,4 @@
+import argparse
 import http.client
 import urllib.request
 import urllib.parse
@@ -5,32 +6,38 @@ import urllib.error
 import base64
 import json
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", help='API token', required=True)
+args = parser.parse_args()
+token = args.t
+
 headers = {
     # Request headers
     # 'Ocp-Apim-Subscription-Key': '{subscription key}',
-    'Authorization': '***',
+    'Authorization': f'Bearer {token}',
     'accept': 'text/plain',
 }
 
 params = urllib.parse.urlencode({
     # Request parameters
     'instCode': 'KAA',
+    'tagNo': '93-ZA-101-SC16'
     # 'groupedInstCode': '{string}',
 })
 
 try:
     conn = http.client.HTTPSConnection('stidapi.equinor.com')
-    conn.request("GET", "/plants?%s" % params, "{body}", headers)
+    conn.request("GET", "/KAA/tag/tag-refs?%s" % params, None, headers)
     response = conn.getresponse()
     data = response.read().decode('utf-8')
     # print(data)
     data = json.loads(data)
-    print(json.dumps(data, indent=4))
+    #print(json.dumps(data, indent=4))
     # print(data[10])
 
-    # for installation in data:
-    #     if installation["instCode"] == "KAA":
-    #         print(installation)
+    for tag in data:
+        if tag["tagType"] == "NP":
+            print(json.dumps(tag, indent=4))
 
     conn.close()
 except Exception as e:
