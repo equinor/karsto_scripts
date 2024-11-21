db3ced7cd22232a932c29cd0aac0abb7ed4661b4
Roy Vegard Ovesen <rovo@equinor.com>
2024-11-21 08:43:30 +0100

    Input files given as arguments

--- python/ipam-check-switch.py
+++ python/ipam-check-switch.py
@@ -12,6 +12,9 @@ def main():
     parser.add_argument("-t", help='API token', required=True)
     parser.add_argument("-i", help='Interactively prompt for resolution to differences', action="store_true")
     parser.add_argument("-a", help='Autoupdate information in IPAM', action="store_true")
+    parser.add_argument("--ports", help='File with ports information', required=True)
+    parser.add_argument("--devices", help='File with devices information', required=True)
+    parser.add_argument("--qrs", help='File with qr codes information', required=True)
     args = parser.parse_args()
     token = args.t
     interactive = args.i
@@ -19,14 +22,13 @@ def main():
 
     if (autoupdate and input("Are you sure you want to automatically update IPAM? (yes,NO) ").lower() != 'yes'):
         quit()
-    #quit()
 
     # Exported ports list from HiVision
-    ports_path = "switch_ports.csv"
+    ports_path = args.ports
     # Exported devices list from HiVision
-    devices_path = "switch_devices.csv"
+    devices_path = args.devices
     # Exported QR codes list from HiVision
-    qr_code_path = "switch_qr_code.csv"
+    qr_code_path = args.qrs
 
     ports = {}
     with open(file=ports_path, mode='r') as file:
@@ -96,6 +98,7 @@ def main():
     }
 
 # Check devices
+    print("# Check Devices")
     for d in devices:
         device_name = devices[d]["System Name"]
 
@@ -144,6 +147,7 @@ def main():
 
 
 # Check ports and interfaces
+    print("# Check ports and interfaces")
     for d in ports:
         params = urllib.parse.urlencode({
             'address': d
@@ -197,7 +201,7 @@ def main():
                         device_port = p
                         continue
                 if (not device_port):
-                    print("Port not found", i["name"])
+                    print("Port not found", d, i["name"])
                     continue
 
                 if (device_port["Port Name"].strip() != i["description"]):
